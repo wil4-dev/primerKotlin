ee6b197d15187d3a20dc9ab1ad03005dcdb20673
Wilson Llivicura <wilsondev4.0@gmail.com>
2022-12-26 17:50:44 -0500

    Commit inicial, creamos un archivo gitignore

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+// @autor : Wilson LLivicura - M4A
+
+fun main(args: Array<String>) {
+    val resultadoLista = ordenarElementos()
+    val listaOrdenada = resultadoLista.sorted()
+    println("LISTA ORDENADA: $listaOrdenada")
+}
+
+fun ordenarElementos() : MutableList<Int>{
+    var datos: MutableList<Int> = mutableListOf()
+    for (valor in 1..10) {
+        print("Ingrese el $valor valor: ")
+        var entradaDato = readln().toInt()
+        datos.add(entradaDato)
+    }
+    return datos
+}
